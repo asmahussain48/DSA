a2dc0a45959c6c80c1ae861bb065e4c590d74e4c
asmahussain48 <asma.hussain.mughal@gmail.com>
2025-09-03 18:52:44 +0500

    initi commit.

--- src/Lab2/Array_mergeSorted.java
+++ src/Lab2/Array_mergeSorted.java
@@ -3,20 +3,47 @@ package Lab2;
 import java.util.List;
 
 public class Array_mergeSorted {
-    public static void main(String[] args) {
 
-        int array1[] = {5, 2, 9};
-        int array2[] = {1, 4, 7};
-        int size1 = array1.length;
-        int size2 = array2.length;
-        int SortedArray[]  = new int[size1+size2];
-        int count = 0;
-        while (count<(size1+size2)){
-            for(int i = 0; i<array1.length; i++){
-                
+        public static void main(String[] args) {
+            int[] arr1 = {5, 2, 9};
+            int[] arr2 = {1, 4, 7};
+
+            int[] merged = mergeArrays(arr1, arr2);
+            System.out.print("[ ");
+            for(int e:merged){
+                System.out.print(e+", ");
             }
+            System.out.print("]");
         }
 
-    }
+
+        public static int[] mergeArrays(int[] arr1, int[] arr2) {
+            int n1 = arr1.length;
+            int n2 = arr2.length;
+            int[] merged = new int[n1 + n2];
+            int size = 0;
+
+            for (int num : arr1) {
+                int j = size - 1;
+                while (j >= 0 && merged[j] > num) {
+                    merged[j + 1] = merged[j];
+                    j--;
+                }
+                merged[j + 1] = num;
+                size++;
+            }
+
+            for (int num : arr2) {
+                int j = size - 1;
+                while (j >= 0 && merged[j] > num) {
+                    merged[j + 1] = merged[j];
+                    j--;
+                }
+                merged[j + 1] = num;
+                size++;
+            }
+
+            return merged;
+        }
 }
 
