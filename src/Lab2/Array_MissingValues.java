adbd705238d5106405c7f71d30ff9c3a833574aa
asmahussain48 <asma.hussain.mughal@gmail.com>
2025-09-03 18:37:08 +0500

    initi commit

---
+++ src/Lab2/Array_MissingValues.java
@@ -0,0 +1,26 @@
+package Lab2;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Array_MissingValues {
+    public static void main(String[] args) {
+        int Array1[]={1, 1};
+        int Array2[] = {2,3,4,1,6,7,1,8,3,6};
+        System.out.println(MissingValue(Array1));
+        System.out.println(MissingValue(Array2));
+    }
+    public static List<Integer> MissingValue(int[] Array){
+            ArrayList<Integer> missingValues = new ArrayList<>();
+            int n = Array.length;
+            boolean vistied [] = new boolean[n+1];
+            for(int i =0; i<n; i++){
+                vistied[Array[i]] = true;
+            }
+            for(int i =1; i<= n; i++){
+                if(!vistied[i]){
+                    missingValues.add(i);
+                }
+            }
+          return missingValues;
+    }
+}
---
+++ src/Lab2/Array_mergeSorted.java
@@ -0,0 +1,22 @@
+package Lab2;
+
+import java.util.List;
+
+public class Array_mergeSorted {
+    public static void main(String[] args) {
+
+        int array1[] = {5, 2, 9};
+        int array2[] = {1, 4, 7};
+        int size1 = array1.length;
+        int size2 = array2.length;
+        int SortedArray[]  = new int[size1+size2];
+        int count = 0;
+        while (count<(size1+size2)){
+            for(int i = 0; i<array1.length; i++){
+                
+            }
+        }
+
+    }
+}
+
