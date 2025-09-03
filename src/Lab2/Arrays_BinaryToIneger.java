a6af5eb9f2157b7a6e3264920e8a12c72240fd19
asmahussain48 <asma.hussain.mughal@gmail.com>
2025-09-03 19:29:08 +0500

    initi commit,

--- src/Lab2/Arrays_BinaryToIneger.java
+++ src/Lab2/Arrays_BinaryToIneger.java
@@ -4,23 +4,30 @@ import java.util.Scanner;
 
 public class Arrays_BinaryToIneger {
     public static void main(String[] args) {
-        System.out.println("Enter a non-negatve Integer");
+        System.out.print("Enter a non-negatve Integer");
         Scanner sc=new Scanner(System.in);
         int num = sc.nextInt();
         int binary[] = new int[8];
-        int power = 1;
-        if(num<255 && num>0){
+        int power = 128;
 
-            for(int i = 7; i>=0; i--){
-                if(num>power){
-                    binary[i] = 1;
-                    num= num-power;
-                    power *= 2;
-                }
+        if (num < 0 || num > 255) {
+            System.out.println("Number out of range!");
+            return;
+        }
+
+        for(int i = 0; i<8 ; i++){
+            if(num >= power){
+                binary[i] = 1;
+                num -= power;
             }
+            else {
+                binary[i] = 0;
+            }
+            power = power/2; //128 to 64 , 64 to 32 and so on
 
 
         }
+        System.out.print("Binary: ");
         for(int e:binary){
             System.out.print(e+"  ");
         }
